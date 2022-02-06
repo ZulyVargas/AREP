b16c2b48a4772531e82f74c7d57d90e98c6f4233
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-02-06 18:13:28 -0500

    Resources : Elementos para la página web

--- SparkWebL2/src/main/java/edu/escuelaing/arep/App.java
+++ SparkWebL2/src/main/java/edu/escuelaing/arep/App.java
@@ -14,15 +14,19 @@ import com.google.gson.Gson;
 public class App 
 {
     public static void main(String[] args) {
-
         port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
-        path("/convert",()->{
+        path("/",()->{
+            //Celsius a Fahrenheit
+
             get("/toFahrenheit/:value", (req, res)->{
+                res.redirect("index.html");
                 res.type("application/json");
                 return new Gson().toJson(TemperatureCalculator.convertCelsiusToFahrenheit(new Celsius(Double.valueOf(req.params(":value")))));
             });
+            //Fahrenheit a Celsius
             get("/toCelsius/:value", (req, res)->{
+                res.redirect("index.html");
                 res.type("application/json");
                 return new Gson().toJson(TemperatureCalculator.convertFahrenheitToCelsius(new Fahrenheit(Double.valueOf(req.params(":value")))));
             });
