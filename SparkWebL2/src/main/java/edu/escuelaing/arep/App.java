0a50c3b614cfd29b98bbf68138783bb94349e67b
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-02-06 20:51:22 -0500

    Celsius a Fahrenheit : Funciona celsius a fahrenheit con js

--- SparkWebL2/src/main/java/edu/escuelaing/arep/App.java
+++ SparkWebL2/src/main/java/edu/escuelaing/arep/App.java
@@ -16,25 +16,24 @@ public class App
     public static void main(String[] args) {
         port(getPort());
         staticFiles.location("/view");
-        get("/hello", (req, res) -> "Hello Heroku");
-        
-        get("/",(request,response)-> {
-                    response.redirect("index.html");
+
+        get("/convert",(request,response)-> {
+            response.redirect("/index.html");
             return null;
         });
+
         //Celsius a Fahrenheit
-        post("/toFahrenheit/:value", (req, res)->{
+        post("/convert/toFahrenheit/", (req, res)->{
             res.type("application/json");
-            return new Gson().toJson(TemperatureCalculator.convertCelsiusToFahrenheit(new Celsius(Double.valueOf(req.params(":value")))));
+            return new Gson().toJson(TemperatureCalculator.convertCelsiusToFahrenheit(new Celsius(Double.valueOf(req.body()))));
         });
         //Fahrenheit a Celsius
-        post("/toCelsius/:value", (req, res)->{
+        post("/convert/toCelsius/", (req, res)->{
             res.type("application/json");
-            return new Gson().toJson(TemperatureCalculator.convertFahrenheitToCelsius(new Fahrenheit(Double.valueOf(req.params(":value")))));
+            return new Gson().toJson(TemperatureCalculator.convertFahrenheitToCelsius(new Fahrenheit(Double.valueOf(req.body()))));
         });
 
-}
-
+    }
     static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
