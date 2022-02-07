96a68ff78f40a1fe16ac64371a36f53c9cf03a45
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-02-06 21:17:01 -0500

    Fahrenheit a Celsius: Funciona fahrenheit a celsius con js

--- SparkWebL2/src/main/resources/view/js/app.js
+++ SparkWebL2/src/main/resources/view/js/app.js
@@ -13,3 +13,19 @@ buttonCelsius.addEventListener('click', function () {
             console.log(error);
         })
 });
+
+var buttonFahrenheit = document.getElementById('button-fahrenheit');
+buttonFahrenheit.addEventListener('click', function () {
+    var fahr = document.getElementById('fahrenheitd').value;
+    console.log(fahr);
+	var url2 = 'http://localhost:4567/convert/toCelsius/';
+    axios.post(url2, fahr)
+        .then(res => {
+            var obj2 = JSON.parse(res.data);
+            $("#celsiusd").text("Celsius: " + obj2);
+            console.log(obj2);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+});
